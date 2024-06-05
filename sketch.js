4764b7b6fcd0865a9139c51f59bc3f18c1a277f9
lorenzo villaggi <lorenzo.villaggi@gmail.com>
2024-06-05 11:07:34 -0400

    Add grid display functions and grid data structure

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
+let g;
+let res = 10;
+let pad = res;
+let i = 0;
+
 function setup() {
   createCanvas(400, 400);
+  g = new Grid(res, width, height, pad);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  // g.display_squares();
+  // g.display_centers();
+
+  
+  if (i >= g.n_cols) {
+    i = 0;
+  }
+  g.display_cell(i,i);
+  i++;
 }
