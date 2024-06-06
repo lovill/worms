9c43ac8c0b2a012395499378a0bd1a28cd67cc9d
lorenzo villaggi <lorenzo.villaggi@gmail.com>
2024-06-06 06:46:19 -0400

    Change maxspeed and maxforce

--- agent.js
+++ agent.js
@@ -6,8 +6,8 @@ class Agent {
         this.grid_pos = createVector(0, 0);
         this.convert_coords_to_grid_cells();
         this.r = 30;
-        this.maxspeed = 3; // Maximum speed
-        this.maxforce = 0.2; // Maximum steering force
+        this.maxspeed = 2; // Maximum speed
+        this.maxforce = 0.4; // Maximum steering force
         this.acceleration = createVector(0, 0);
         this.velocity = createVector(0, 0);
 
@@ -33,12 +33,16 @@ class Agent {
 
         let separateForce = this.separate(agents);
         let seekForce = this.seek(createVector(mouseX, mouseY));
+        let bounce_walls = this.boundaries();
     
-        separateForce.mult(slider1.value());
-        seekForce.mult(slider2.value());
+        separateForce.mult(sep_slider1.value());
+        seekForce.mult(seek_slider2.value());
+        // console.log(bounce_walls);
+        bounce_walls.mult(1.);
     
         this.applyForce(separateForce);
         this.applyForce(seekForce);
+        this.applyForce(bounce_walls);
     }
     
     applyForce(force) {
@@ -49,7 +53,7 @@ class Agent {
     // Separation
     // Method checks for nearby vehicles and steers away
     separate(vehicles) {
-        let desiredseparation = slider3.value();
+        let desiredseparation = des_sep_slider3.value();
         let sum = createVector();
         let count = 0;
         // For every boid in the system, check if it's too close
@@ -81,7 +85,8 @@ class Agent {
   // A method that calculates a steering force towards a target
   // STEER = DESIRED MINUS VELOCITY
     seek(target) {
-        let desired = p5.Vector.sub(target, this.coords); // A vector pointing from the location to the target
+        // let desired = p5.Vector.sub(target, this.coords); // A vector pointing from the location to the target
+        let desired = p5.Vector.sub(this.coords, target); // A vector pointing from the location to the target
 
         // Normalize desired and scale to maximum speed
         desired.normalize();
@@ -112,14 +117,14 @@ class Agent {
     }
 
     display() {
-        console.log(
-            this.grid_pos.x,
-            this.grid_pos.y
-        )
-        console.log(
-            this.coords.x,
-            this.coords.y
-        )
+        // console.log(
+        //     this.grid_pos.x,
+        //     this.grid_pos.y
+        // )
+        // console.log(
+        //     this.coords.x,
+        //     this.coords.y
+        // )
         this.grid.display_cell(
             this.grid_pos.x,
             this.grid_pos.y
@@ -139,4 +144,57 @@ class Agent {
             this.coords.y = this.r;
         }
     }
+
+    borders_within() {
+        if (this.coords.x < this.r) {
+            this.coords.x = this.r;
+            this.velocity.x *= -1;
+        }
+        if (this.coords.y < this.r) {
+            this.coords.y = this.r;
+            this.velocity.y *= -1;
+        }
+        if (this.coords.x > width - this.r) {
+            this.coords.x = width - this.r;
+            this.velocity.x *= -1;
+        }
+        if (this.coords.y > height - this.r) {
+            this.coords.y = height - this.r;
+            this.velocity.y *= -1;
+        }
+    }
+
+    boundaries() {
+
+        let desired = null;
+        let steer = createVector(0,0);
+        // console.log(steer);
+        if (this.coords.x < this.r) {
+            this.coords.x = this.r;
+            desired = createVector(this.maxspeed, this.velocity.y);
+        } else if (this.coords.x > width - this.r) {
+            this.coords.x = width - this.r;
+            desired = createVector(-this.maxspeed, this.velocity.y);
+        }
+    
+        if (this.coords.y < this.r) {
+            this.coords.y = this.r;
+            desired = createVector(this.velocity.x, this.maxspeed);
+        } else if (this.coords.y > height - this.r) {
+            this.coords.y = height - this.r;
+            desired = createVector(this.velocity.x, -this.maxspeed);
+        }
+        // console.log(steer);
+        
+        if (desired !== null) {
+          desired.normalize();
+          desired.mult(this.maxspeed);
+          steer = p5.Vector.sub(desired, this.velocity);
+          steer.limit(this.maxforce);
+            // console.log(steer);
+
+        //   this.applyForce(steer);
+        }
+        return steer;
+      }
 }
