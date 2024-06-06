68bffc11c14491b9290120e1dc65e6d9986345e9
lorenzo villaggi <lorenzo.villaggi@gmail.com>
2024-06-06 06:46:45 -0400

    Update grid res and update sliders

--- sketch.js
+++ sketch.js
@@ -1,14 +1,15 @@
 let g;
 let a;
-let res = 8;
+let res = 2;
 let pad = res;
 let i = 0;
 
-let slider1;
-let slider2;
-let slider3;
+let sep_slider1;
+let seek_slider2;
+let des_sep_slider3;
 
 let agents = [];
+let n_agents = 800;
 
 function setup() {
   createCanvas(400, 400);
@@ -16,9 +17,19 @@ function setup() {
   a1 = new Agent(width/2, height/2, g);
   a2 = new Agent(300, height/2, g);
 
-  slider1 = createSlider(0, 8, 4);
-  slider2 = createSlider(0, 8, 4);
-  slider3 = createSlider(10, 160, 24);
+  sep_slider1 = createSlider(0.1, 8, 5);
+  seek_slider2 = createSlider(0.01, 5, 2);
+  des_sep_slider3 = createSlider(0.01, 80, 10);
+
+  for (let i = 0; i < n_agents; i++) {
+    agents.push(
+      new Agent(
+          random(width), 
+          random(height),
+          g
+        )
+      );
+  }
 }
 
 function draw() {
@@ -27,23 +38,14 @@ function draw() {
   // g.display_centers();
 
   // a1.wave_move();
-  a1.applyBehaviors(a2);
-  a1.update();
-  a1.borders();
-  a1.convert_coords_to_grid_cells();
 
-  a2.applyBehaviors(a1);
-  a2.update();
-  a2.borders();
-  a2.convert_coords_to_grid_cells();
-  
-  a1.display();
-  a2.display();
+  for (let a of agents) {
+    a.applyBehaviors(agents);
+    a.update();
+    // a.borders();
+    // a.borders_within();
+    a.convert_coords_to_grid_cells();
+    a.display();
+  }
 
-
-  // if (i >= g.n_cols) {
-  //   i = 0;
-  // }
-  // g.display_cell(i,i);
-  // i++;
 }
